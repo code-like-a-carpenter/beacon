398ba8f9dc4494e1966a7e88e57b7c990813c091
ianwremmel <1182361+ianwremmel@users.noreply.github.com>
2022-08-10 19:15:06 -0700

    feat(telemetry): generalize running code in a span

--- packages/@beacon/interact/src/interact.ts
+++ packages/@beacon/interact/src/interact.ts
@@ -1,4 +1,6 @@
-import {trace, SpanKind, SpanStatusCode} from '@opentelemetry/api';
+import {SpanKind} from '@opentelemetry/api';
+
+import {runWithNewSpan} from '@beacon/telemetry';
 
 import {Context, Interactor} from './types';
 
@@ -10,25 +12,10 @@ export async function interact<T, R>(
   args: T,
   context: Context
 ): Promise<R> {
-  return trace
-    .getTracer('default')
-    .startActiveSpan(
-      interactor.name ?? 'unknown interactor',
-      {kind: SpanKind.INTERNAL},
-      async (span) => {
-        try {
-          return await interactor(args, context);
-        } catch (err) {
-          const error =
-            err instanceof Error
-              ? err
-              : new Error(typeof err === 'string' ? err : undefined);
-          span.setStatus({code: SpanStatusCode.ERROR, message: error.message});
-          span.recordException(error);
-          throw err;
-        } finally {
-          span.end();
-        }
-      }
-    );
+  const logger = context.logger.child({interactor: interactor.name});
+
+  return runWithNewSpan(
+    {kind: SpanKind.INTERNAL, name: interactor.name ?? 'unknown interactor'},
+    () => interactor(args, {...context, logger})
+  );
 }
